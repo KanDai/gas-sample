99a54c89a93296c6930b542781d09f2c34a34240
KanDai <KanDai@users.noreply.github.com>
2023-11-24 10:28:55 +0900

    update script

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-Google Apps Script development environment using [clasp](https://github.com/google/clasp#create)
-
-## Hou to use
-
-```
-npm install
-```
-
-```
-npm run init -title="YOUR_SCRIPT_NAME"
-```
+A列とB列に記載したユーザー情報からランダムで選んでSlackにPOSTするサンプル
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,38 @@
-const test = () => {
-  console.log('Hello World!')
+const getUser = () => {
+  const sheet = SpreadsheetApp.getActiveSheet()
+  const lastRow = sheet.getLastRow()
+  const range = sheet.getRange(2, 1, lastRow - 1, 2)
+  const values = range.getValues()
+  const randomIndex = Math.floor(Math.random() * values.length)
+  return values[randomIndex]
+}
+
+const getProperty = (property: string) => {
+  return PropertiesService.getScriptProperties().getProperty(property)
+}
+
+const postResult = (username: string, user_id: string) => {
+  const url = getProperty('SLACK_WEBHOOK_URL')
+  const data = {
+    channel: getProperty('SLACK_POST_CHANNEL'),
+    username: getProperty('SLACK_POST_USER'),
+    attachments: [
+      {
+        text: `結果は${username}です。<@${user_id}>`,
+      },
+    ],
+  }
+  const payload = JSON.stringify(data)
+  const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    method: 'post',
+    contentType: 'application/json',
+    payload: payload,
+  }
+  if (!url) return
+  UrlFetchApp.fetch(url, options)
+}
+
+const main = () => {
+  const user = getUser()
+  postResult(user[0], user[1])
 }
